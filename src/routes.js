c27d97e32ac56352c9e99342c1eb318680a9af7e
leomoraiscam <leozonnn@gmail.com>
2020-07-23 16:53:47 -0500

    Implementando o jest no projeto e configurando o mesmo para os testes

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,4 @@
 import { Router } from "express";
 const routes = new Router();
 
-import User from "./app/models/User";
-
-User.create({
-  name: "leonardo",
-  email: "email@email.com.br",
-  password_hash: "oiuytrtyuionjbhv",
-});
-
 export default routes;
